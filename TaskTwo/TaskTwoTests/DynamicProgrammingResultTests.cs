ece5925a927b38abc3f2fad138d275cd7499d329
Krzysztof Mierzejewski <kmierz@gmail.com>
2022-06-14 15:58:48 +0200

    Dynamic Programming Result unit tests.

---
+++ TaskTwo/TaskTwoTests/DynamicProgrammingResultTests.cs
@@ -0,0 +1,157 @@
+using Microsoft.Toolkit.HighPerformance;
+using NUnit.Framework;
+
+namespace TaskTwoTests
+{
+    using TaskTwo;
+    using TaskTwo.Contracts;
+    
+    public sealed class DynamicProgrammingResultTests
+    {
+        private ILargestSquare _algorithm;
+        
+        [SetUp]
+        public void Setup()
+        {
+            this._algorithm = new DynamicProgramming();
+        }
+
+        [Test]
+        public void LargestSquare_EmptyMatrix_Zero()
+        {
+            // Arrange
+            var matrix = new bool[,] { };
+            // Act
+            var result = this._algorithm.GetLargestSquareArea(matrix);
+            // Assert
+            Assert.Zero(result);
+        }
+        
+        [Test]
+        public void LargestSquare_ZeroHeight_Zero()
+        {
+            // Arrange
+            var matrix = new bool[0, 10];
+            // Act
+            var result = this._algorithm.GetLargestSquareArea(matrix);
+            // Assert
+            Assert.Zero(result);
+        }
+        
+        [Test]
+        public void LargestSquare_ZeroWidth_Zero()
+        {
+            // Arrange
+            var matrix = new bool[5, 0];
+            // Act
+            var result = this._algorithm.GetLargestSquareArea(matrix);
+            // Assert
+            Assert.Zero(result);
+        }
+        
+        [Test]
+        public void LargestSquare_AllZero_Zero()
+        {
+            // Arrange
+            var matrix = new bool[5, 10];
+            // Act
+            var result = this._algorithm.GetLargestSquareArea(matrix);
+            // Assert
+            Assert.Zero(result);
+        }
+        
+        [Test]
+        public void LargestSquare_AllOneSquare_SideSquared()
+        {
+            // Arrange
+            const int sideLength = 10;
+            Span2D<bool> matrix = new bool[sideLength, sideLength];
+            matrix.Fill(true);
+            // Act
+            var result = this._algorithm.GetLargestSquareArea(matrix);
+            // Assert
+            Assert.AreEqual(result, sideLength * sideLength);
+        }
+        
+        [Test]
+        public void LargestSquare_AllOneVerticalRectangle_ShortSideSquared()
+        {
+            // Arrange
+            const int sideLength = 10;
+            Span2D<bool> matrix = new bool[sideLength+5, sideLength];
+            matrix.Fill(true);
+            // Act
+            var result = this._algorithm.GetLargestSquareArea(matrix);
+            // Assert
+            Assert.AreEqual(result, sideLength * sideLength);
+        }
+        
+        [Test]
+        public void LargestSquare_AllOneHorizontalRectangle_ShortSideSquared()
+        {
+            // Arrange
+            const int sideLength = 10;
+            Span2D<bool> matrix = new bool[sideLength, sideLength+5];
+            matrix.Fill(true);
+            // Act
+            var result = this._algorithm.GetLargestSquareArea(matrix);
+            // Assert
+            Assert.AreEqual(result, sideLength * sideLength);
+        }
+        
+        [Test]
+        public void LargestSquare_ZeroPerimeter_InnerSideSquared()
+        {
+            // Arrange
+            const int sideLength = 10;
+            Span2D<bool> matrix = new bool[sideLength+2, sideLength+2];
+            matrix[1..^1, 1..^1].Fill(true);
+            // Act
+            var result = this._algorithm.GetLargestSquareArea(matrix);
+            // Assert
+            Assert.AreEqual(result, sideLength * sideLength);
+        }
+        
+        [Test]
+        public void LargestSquare_SeparateSameSizeSquares_OneSideSquared()
+        {
+            // Arrange
+            const int sideLength = 5;
+            Span2D<bool> matrix = new bool[2*sideLength+3, 2*sideLength+2];
+            matrix[1..(sideLength+1), ..sideLength].Fill(true);
+            matrix[(sideLength+2)..(2*sideLength+2), (sideLength+1)..(2*sideLength+1)].Fill(true);
+            // Act
+            var result = this._algorithm.GetLargestSquareArea(matrix);
+            // Assert
+            Assert.AreEqual(result, sideLength * sideLength);
+        }
+        
+        [Test]
+        public void LargestSquare_SeparateDifferentSizeSquares_LargestSquareArea()
+        {
+            // Arrange
+            const int sideLength = 5;
+            Span2D<bool> matrix = new bool[2*sideLength+3, 2*sideLength+2];
+            matrix[1..(sideLength+1), ..sideLength].Fill(true);  // large square
+            matrix[(sideLength+2)..(2*sideLength), (sideLength+1)..(2*sideLength)].Fill(true); // small square
+            // Act
+            var result = this._algorithm.GetLargestSquareArea(matrix);
+            // Assert
+            Assert.AreEqual(result, sideLength * sideLength);
+        }
+        
+        [Test]
+        public void LargestSquare_OverlappingFigures_SquareArea()
+        {
+            // Arrange
+            const int sideLength = 8;
+            Span2D<bool> matrix = new bool[7*sideLength/4, sideLength+2];
+            matrix[..sideLength, 1..(sideLength+1)].Fill(true); // square
+            matrix[.., 1..(sideLength/2)].Fill(true); // overlapping vertical rectangle
+            // Act
+            var result = this._algorithm.GetLargestSquareArea(matrix);
+            // Assert
+            Assert.AreEqual(result, sideLength * sideLength);
+        }
+    }
+}
