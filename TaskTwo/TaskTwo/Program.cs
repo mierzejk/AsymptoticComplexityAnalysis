d44745231aa3b87a9b9743c5ddcb6c595835ac7f
Krzysztof Mierzejewski <kmierz@gmail.com>
2022-06-14 13:35:23 +0200

    Better tests.

--- TaskTwo/TaskTwo/Program.cs
+++ TaskTwo/TaskTwo/Program.cs
@@ -5,7 +5,9 @@ using TaskTwo.Utils;
 
 var a = new DynamicProgramming();
 bool[,] b = {{false, true}, {true, false}};
-uint[,] v = {{1, 0, 1, 0, 0}, {1, 0, 1, 1, 1}, {1, 1, 1, 1, 1}, {1, 0, 0, 1, 0}};
+uint[,] v = {{1, 0, 1, 0, 0}, {1, 0, 1, 1, 1}, {1, 1, 1, 1, 1}, {1, 0, 0, 1, 0},
+    {0, 1, 1, 1, 1}, {0, 1, 1, 1, 1}, {0, 1, 1, 1, 1}, {0, 1, 1, 1, 1}, {0, 1, 1, 1, 1},
+    {1, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}};
 int[,] vv = {{1, 0, 1, 0, 0}, {1, 1, 1, 1, 0}, {1, 1, 1, 1, 1}, {0, 0, 0, 0, 0}};
 Console.WriteLine(a.GetLargestSquareArea(new bool[0, 0]));
 Console.WriteLine(a.GetLargestSquareArea(new bool[4, 2]));
