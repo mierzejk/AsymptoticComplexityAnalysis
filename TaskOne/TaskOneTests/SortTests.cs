7be4057ad963767628d4110fdd200bec256ab67b
Krzysztof Mierzejewski <kmierz@gmail.com>
2022-06-15 20:03:46 +0200

    SortTests completed.

--- TaskOne/TaskOneTests/SortTests.cs
+++ TaskOne/TaskOneTests/SortTests.cs
@@ -23,4 +23,90 @@ public abstract class SortTests
         // Act & Assert
         Assert.Catch<ArgumentNullException>(() => this.AscendingSort(input!));
     }
+    
+    [Test]
+    public void Sort_Empty_Empty()
+    {
+        // Arrange
+        var input = Array.Empty<double>();
+        // Act
+        var result = this.AscendingSort(input);
+        // Assert
+        CollectionAssert.IsEmpty(result);
+    }
+    
+    [Test]
+    public void Sort_OneElement_OneElement()
+    {
+        // Arrange
+        var input = new List<double> { 3.141592654D };
+        // Act
+        var result = this.AscendingSort(input);
+        // Assert
+        Assert.That(result, Has.Length.EqualTo(1));
+        CollectionAssert.AreEqual(input, result);
+    }
+    
+    [Test]
+    public void Sort_UniqueCollection_SortedAscending()
+    {
+        // Arrange
+        var input = new List<double> { 8D, 12.5D, 1024.5D, -0D, -6D, 0D };
+        // Act
+        var result = this.AscendingSort(input);
+        // Assert
+        Assert.That(result, Has.Length.EqualTo(input.Count));
+        CollectionAssert.AreEqual(new[] { -6D, -0D, 0D, 8D, 12.5D, 1024.5D }, result);
+    }
+    
+    [Test]
+    public void SortReversed_UniqueCollection_SortedDescending()
+    {
+        // Arrange
+        var input = new List<double> { 8D, 12.5D, 1024.5D, -0D, -6D, 0D };
+        // Act
+        var result = this.ReversedSort(input);
+        // Assert
+        Assert.That(result, Has.Length.EqualTo(input.Count));
+        CollectionAssert.AreEqual(new[] { 1024.5D, 12.5D, 8D, 0D, -0D, -6D }, result);
+    }
+    
+    [Test]
+    public void Sort_DuplicatedItems_SortedAscending()
+    {
+        // Arrange
+        var input = new List<double> { 8D, 12.5D, 8D, 12.5D, -8D, 8D };
+        // Act
+        var result = this.AscendingSort(input);
+        input.Sort();
+        // Assert
+        Assert.That(result, Has.Length.EqualTo(input.Count));
+        CollectionAssert.AreEqual(input, result);
+    }
+    
+    [Test]
+    public void SortReversed_DuplicatedItems_SortedDescending()
+    {
+        // Arrange
+        var input = new List<double> { 8D, 12.5D, 8D, 12.5D, -8D, 8D };
+        // Act
+        var result = this.ReversedSort(input);
+        input.Sort();
+        input.Reverse();
+        // Assert
+        Assert.That(result, Has.Length.EqualTo(input.Count));
+        CollectionAssert.AreEqual(input, result);
+    }
+    
+    [Test]
+    public void Sort_Collection_NotInPlace()
+    {
+        // Arrange
+        var input = new [] { 3.141592654D, 9.80665D, 2.71828D };
+        // Act
+        var result = this.AscendingSort(input);
+        // Assert
+        Assert.That(input, Has.Length.EqualTo(result.Length));
+        CollectionAssert.AreNotEqual(input, result);
+    }
 }
